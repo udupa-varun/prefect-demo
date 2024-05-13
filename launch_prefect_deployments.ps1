ea3a4a6a3de76ac714c26b7391c52d882ffbb28b
Varun Udupa <udupa.varun@gmail.com>
2024-05-13 16:48:57 -0400

    clean up deployments launcher

--- launch_prefect_deployments.ps1
+++ launch_prefect_deployments.ps1
@@ -19,9 +19,6 @@ Invoke-Expression ".\prefect-env\Scripts\Activate.ps1"
 
 # Force-correct API URL
 Invoke-Expression "prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api"
-# Invoke-Expression "prefect config set PREFECT_API_URL=https://api.prefect.cloud/api/accounts/2a53db77-8a31-48b5-b1fa-015b9c3afc61/workspaces/5f23e60f-8b28-4c97-9718-3fb495e81360"
-
-# Invoke-Expression "prefect config unset PREFECT_LOGGING_SETTINGS_PATH"
 
 # overwrite active deployments
 Invoke-Expression "python .\serve_deployments.py"
